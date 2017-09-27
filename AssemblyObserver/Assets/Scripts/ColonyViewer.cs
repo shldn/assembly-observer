f7563eb5c0869407643a82053d102799d787781f
Erik Hill <eghill@ucsd.edu>
2017-09-27 11:35:12 -0700

    Updated ColonyViewer to match new interface for getPositions.

--- AssemblyObserver/Assets/Scripts/ColonyViewer.cs
+++ AssemblyObserver/Assets/Scripts/ColonyViewer.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Assembly;
+using System;
 
 public class ColonyViewer : MonoBehaviour {
 
     public int size = 10;
     public GameObject visual = null;
     private Colony colony = null;
-    private List<GameObject> visuals = new List<GameObject>();
+    private Dictionary<int, GameObject> visuals = new Dictionary<int, GameObject>();
 	
 	void Start () {
         ConsoleRedirect.Redirect();
@@ -17,14 +18,18 @@ public class ColonyViewer : MonoBehaviour {
     }
 	
 	void Update () {
-        List<System.Numerics.Vector3> positions = colony.GetPositions();
-        for (int i = 0; i < visuals.Count; ++i)
-            visuals[i].transform.position = Vec3(positions[i]);
+        List<System.Tuple<int,System.Numerics.Vector3>> positionData = colony.GetPositions();
+        GameObject visual = null;
+        for (int i = 0; i < positionData.Count; ++i)
+        {
+            if (visuals.TryGetValue(positionData[i].Item1, out visual))
+                visual.transform.position = Vec3(positionData[i].Item2);
+        }
 	}
 
     void OnAssemblyCreation(Assembly.Assembly assembly)
     {
-        visuals.Add(GameObject.Instantiate(visual));
+        visuals.Add(assembly.id, GameObject.Instantiate(visual));
     }
 
     Vector3 Vec3(System.Numerics.Vector3 v) {
