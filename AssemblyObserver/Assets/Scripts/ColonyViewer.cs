be70c458f96d47325dc9d9493507a18ddc7be9aa
Erik Hill <eghill@ucsd.edu>
2017-09-27 10:48:00 -0700

    ColonyViewer creates visuals on the assembly creation event.

--- AssemblyObserver/Assets/Scripts/ColonyViewer.cs
+++ AssemblyObserver/Assets/Scripts/ColonyViewer.cs
@@ -9,20 +9,23 @@ public class ColonyViewer : MonoBehaviour {
     public GameObject visual = null;
     private Colony colony = null;
     private List<GameObject> visuals = new List<GameObject>();
-	// Use this for initialization
+	
 	void Start () {
         ConsoleRedirect.Redirect();
+        Assembly.Assembly.onCreation += OnAssemblyCreation;
         colony = Colony.Create(size);
-        for (int i = 0; i < size; ++i)
-            visuals.Add(GameObject.Instantiate(visual));
     }
 	
-	// Update is called once per frame
 	void Update () {
         List<System.Numerics.Vector3> positions = colony.GetPositions();
         for (int i = 0; i < visuals.Count; ++i)
             visuals[i].transform.position = Vec3(positions[i]);
 	}
+
+    void OnAssemblyCreation(Assembly.Assembly assembly)
+    {
+        visuals.Add(GameObject.Instantiate(visual));
+    }
 
     Vector3 Vec3(System.Numerics.Vector3 v) {
         return new Vector3(v.X, v.Y, v.Z);
