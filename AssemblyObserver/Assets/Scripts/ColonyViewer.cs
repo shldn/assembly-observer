4506a34b52dac48dd7bfa3040340f310249dfa22
Wes <wes.r.hawkins@gmail.com>
2017-10-05 14:19:10 -0700

    Assembly node structure

--- AssemblyObserver/Assets/Scripts/ColonyViewer.cs
+++ AssemblyObserver/Assets/Scripts/ColonyViewer.cs
@@ -22,30 +22,36 @@ public class ColonyViewer : MonoBehaviour {
         GameObject visual = null;
         for (int i = 0; i < positionData.Count; ++i)
         {
-            if (visuals.TryGetValue(positionData[i].Item1, out visual))
+            if (visuals.TryGetValue(positionData[i].Item1, out visual)){
                 visual.transform.position = Vec3(positionData[i].Item2);
+				Debug.DrawRay(visual.transform.position, visual.transform.forward * 100f);
+			}
         }
 	}
 
     void OnAssemblyCreation(Assembly.Assembly assembly)
     {
-        GameObject assemblyObj = new GameObject("Assembly-" + assembly.id);
-        assemblyObj.transform.position = Vec3(assembly.position);
-        for (int i = 0; i < assembly.nodes.Count; ++i) {
+        GameObject assemblyObj = new GameObject("Assembly-" + assembly.Id);
+        assemblyObj.transform.position = Vec3(assembly.Position);
+        for (int i = 0; i < assembly.NodesList.Count; ++i) {
             GameObject node = GameObject.Instantiate(visual, assemblyObj.transform);
-            node.transform.localPosition = Vec3(assembly.nodes[i].localHexPos);
+            node.transform.localPosition = Vec3(HexUtilities.HexToWorld(assembly.NodesList[i].LocalHexPos)) - Vec3(assembly.LocalCenterOfMass);
         }
+		assemblyObj.transform.rotation = Quat(assembly.Rotation);
 
-        visuals.Add(assembly.id, assemblyObj);
+        visuals.Add(assembly.Id, assemblyObj);
     }
 
     Vector3 Vec3(System.Numerics.Vector3 v) {
         return new Vector3(v.X, v.Y, v.Z);
     }
 
-    Vector3 Vec3(Triplet v)
-    {
+    Vector3 Vec3(Triplet v){
         return new Vector3(v.x, v.y, v.z);
+    }
+
+	Quaternion Quat(System.Numerics.Quaternion q) {
+        return new Quaternion(q.X, q.Y, q.Z, q.W);
     }
 
 }
