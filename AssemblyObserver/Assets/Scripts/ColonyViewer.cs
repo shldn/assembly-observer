934e0835515cf225e01eac86162674423189f3ef
Wes <wes.r.hawkins@gmail.com>
2017-10-10 14:19:59 -0700

    Some physics, node types, math

--- AssemblyObserver/Assets/Scripts/ColonyViewer.cs
+++ AssemblyObserver/Assets/Scripts/ColonyViewer.cs
@@ -1,59 +1,157 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Assembly;
+using EGL;
 using System;
 
 public class ColonyViewer : MonoBehaviour {
 
-    public int size = 10;
-    public GameObject visual = null;
-    public float speed = 0.001f;
+	public static ColonyViewer Inst = null;
+
+    [SerializeField] private int size = 10;
     private Colony colony = null;
-    private Dictionary<int, GameObject> visuals = new Dictionary<int, GameObject>();
-	
-	void Start () {
-        ConsoleRedirect.Redirect();
-        Assembly.Assembly.onCreation += OnAssemblyCreation;
-        colony = Colony.Create(size);
-    }
-	
-	void Update () {
-        List<System.Tuple<int,System.Numerics.Vector3>> positionData = colony.GetPositions();
-        GameObject visual = null;
-        for (int i = 0; i < positionData.Count; ++i)
-        {
-            if (visuals.TryGetValue(positionData[i].Item1, out visual)){
-                visual.transform.position = Vec3(positionData[i].Item2);
-				Debug.DrawRay(visual.transform.position, visual.transform.forward * 100f);
+
+	[SerializeField] private GameObject nodeVisual = null; public GameObject NodeVisual { get { return nodeVisual; } }
+	[SerializeField] private Color basicNodeColor = Color.gray;
+	[SerializeField] private Color detectorNodeColor = Color.green;
+	[SerializeField] private Color processorNodeColor = Color.blue;
+	[SerializeField] private Color motorNodeColor = Color.red;
+
+
+
+	private List<AssemblyViewer> assemblyViewers = new List<AssemblyViewer>();
+	public class AssemblyViewer {
+		private Assembly assembly = null;
+		private GameObject gameObject = null;
+		private NodeViewer[] nodeViewers = null;
+
+		public AssemblyViewer(Assembly assembly){
+			this.assembly = assembly;
+			Inst.assemblyViewers.Add(this);
+
+			// Create assembly container.
+			gameObject = new GameObject("Assembly-" + assembly.Id);
+			gameObject.transform.position = Vec3(assembly.Position);
+			gameObject.transform.rotation = Quat(assembly.Rotation);
+
+			// Create nodes.
+			nodeViewers = new NodeViewer[assembly.NodesList.Count];
+			for (int i = 0; i < assembly.NodesList.Count; ++i)
+				nodeViewers[i] = new NodeViewer(assembly.NodesList[i], assembly, gameObject.transform);
+			
+			// DEBUG - randomized initiial velocities
+			Vector3 randomVelocity = UnityEngine.Random.insideUnitSphere * 1f;
+			Vector3 randomAngularVelocity = UnityEngine.Random.insideUnitSphere * 1f;
+
+			assembly.ApplyForce(randomVelocity.x, randomVelocity.y, randomVelocity.z);
+			assembly.ApplyTorque(randomAngularVelocity.x, randomAngularVelocity.y, randomAngularVelocity.z);
+
+		} // End of Ctor.
+
+		public void Update(){
+			gameObject.transform.position = Vec3(assembly.Position);
+			gameObject.transform.rotation = Quat(assembly.Rotation);
+
+			Debug.DrawRay(Vec3(assembly.Position), Quat(assembly.Rotation) * Vector3.forward * 2f, Color.blue);
+			Debug.DrawRay(Vec3(assembly.Position), Quat(assembly.Rotation) * Vector3.right * 2f, Color.green);
+			Debug.DrawRay(Vec3(assembly.Position), Quat(assembly.Rotation) * Vector3.up * 2f, Color.red);
+
+
+			// DEBUG - drive around assembly
+			Vector3 throttle = Vector3.zero;
+			if(Input.GetKey(KeyCode.Keypad4))
+				throttle.x += 1f;
+			if(Input.GetKey(KeyCode.Keypad6))
+				throttle.x -= 1f;
+			if(Input.GetKey(KeyCode.Keypad8))
+				throttle.y += 1f;
+			if(Input.GetKey(KeyCode.Keypad5))
+				throttle.y -= 1f;
+			if(Input.GetKey(KeyCode.Keypad7))
+				throttle.z += 1f;
+			if(Input.GetKey(KeyCode.Keypad9))
+				throttle.z -= 1f;
+			assembly.ApplyForce(throttle.x * Inst.colony.TimeStep, throttle.y * Inst.colony.TimeStep, throttle.z * Inst.colony.TimeStep);
+
+			Vector3 rotationThrottle = Vector3.zero;
+			if(Input.GetKey(KeyCode.A))
+				rotationThrottle.x += 1f;
+			if(Input.GetKey(KeyCode.D))
+				rotationThrottle.x -= 1f;
+			if(Input.GetKey(KeyCode.W))
+				rotationThrottle.y += 1f;
+			if(Input.GetKey(KeyCode.S))
+				rotationThrottle.y -= 1f;
+			if(Input.GetKey(KeyCode.Q))
+				rotationThrottle.z += 1f;
+			if(Input.GetKey(KeyCode.E))
+				rotationThrottle.z -= 1f;
+			assembly.ApplyTorque(rotationThrottle.x * Inst.colony.TimeStep, rotationThrottle.y * Inst.colony.TimeStep, rotationThrottle.z * Inst.colony.TimeStep);
+			
+		} // End of Update().
+
+	} // End of AssemblyViewer.
+
+
+	public class NodeViewer {
+		private Node node = null;
+		private Assembly assembly = null;
+		private GameObject gameObject = null;
+
+		public NodeViewer(Node node, Assembly assembly, Transform parent){
+			this.node = node;
+			this.assembly = assembly;
+			gameObject = Instantiate(Inst.nodeVisual, parent);
+			gameObject.transform.localPosition = Vec3(node.LocalRealPos);
+
+			Color nodeColor;
+			switch(node.MyNodeType){
+				case NodeType.detector : nodeColor = Inst.detectorNodeColor; break;
+				case NodeType.processor : nodeColor = Inst.processorNodeColor; break;
+				case NodeType.motor : nodeColor = Inst.motorNodeColor; break;
+				default : nodeColor = Inst.basicNodeColor; break;
 			}
-        }
-        colony.speed = speed;
-	}
+			gameObject.GetComponent<Renderer>().material.color = nodeColor;
+			gameObject.GetComponent<TrailRenderer>().startColor = new Color(nodeColor.r, nodeColor.g, nodeColor.b, 0.5f);
+			gameObject.GetComponent<TrailRenderer>().endColor = new Color(nodeColor.r, nodeColor.g, nodeColor.b, 0f);
+		} // Ctor.
 
-    void OnAssemblyCreation(Assembly.Assembly assembly)
-    {
-        GameObject assemblyObj = new GameObject("Assembly-" + assembly.Id);
-        assemblyObj.transform.position = Vec3(assembly.Position);
-        for (int i = 0; i < assembly.NodesList.Count; ++i) {
-            GameObject node = GameObject.Instantiate(visual, assemblyObj.transform);
-            node.transform.localPosition = Vec3(HexUtilities.HexToWorld(assembly.NodesList[i].LocalHexPos)) - Vec3(assembly.LocalCenterOfMass);
-        }
-		assemblyObj.transform.rotation = Quat(assembly.Rotation);
+		public void Update(){
+		} // End of Update().
 
-        visuals.Add(assembly.Id, assemblyObj);
-    }
+	} // End of NodeViewer.
 
-    Vector3 Vec3(System.Numerics.Vector3 v) {
+
+	private void Awake() {
+		Inst = this;
+	} // End of Awake().
+
+	private void Start () {
+        ConsoleRedirect.Redirect();
+        Assembly.onCreation += OnAssemblyCreation;
+        colony = Colony.Create(size);
+    } // End of Start().
+	
+	private void Update () {
+        for (int i = 0; i < assemblyViewers.Count; i++)
+			assemblyViewers[i].Update();
+	} // End of Update().
+
+    private void OnAssemblyCreation(Assembly assembly){
+        new AssemblyViewer(assembly);
+    } // End of OnAssemblyCreation().
+
+
+    public static Vector3 Vec3(System.Numerics.Vector3 v) {
         return new Vector3(v.X, v.Y, v.Z);
     }
 
-    Vector3 Vec3(Triplet v){
+    public static Vector3 Vec3(Triplet v){
         return new Vector3(v.x, v.y, v.z);
     }
 
-	Quaternion Quat(System.Numerics.Quaternion q) {
+	public static Quaternion Quat(System.Numerics.Quaternion q) {
         return new Quaternion(q.X, q.Y, q.Z, q.W);
     }
 
-}
+} // End of ColonyViewer.
