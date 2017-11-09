811bf962d532f1e5e65ca545d51a82f01e16fe5c
Wes <wes.r.hawkins@gmail.com>
2017-11-09 14:47:45 -0800

    Fancy vision cones

--- AssemblyObserver/Assets/Scripts/ColonyViewer.cs
+++ AssemblyObserver/Assets/Scripts/ColonyViewer.cs
@@ -12,6 +12,7 @@ public class ColonyViewer : MonoBehaviour {
     private Colony colony = null;
 
 	[SerializeField] private GameObject nodeVisual = null; public GameObject NodeVisual { get { return nodeVisual; } }
+	[SerializeField] private GameObject sensorConeVisual = null; public GameObject SensorConeVisual { get { return nodeVisual; } }
 	[SerializeField] private Color basicNodeColor = Color.gray;
 	[SerializeField] private Color detectorNodeColor = Color.green;
 	[SerializeField] private Color processorNodeColor = Color.blue;
@@ -114,6 +115,15 @@ public class ColonyViewer : MonoBehaviour {
 			gameObject.GetComponent<Renderer>().material.color = nodeColor;
 			gameObject.GetComponent<TrailRenderer>().startColor = new Color(nodeColor.r, nodeColor.g, nodeColor.b, 0.5f);
 			gameObject.GetComponent<TrailRenderer>().endColor = new Color(nodeColor.r, nodeColor.g, nodeColor.b, 0f);
+
+			// Add sensor cone
+			if(node.MyNodeType == NodeType.detector){
+				GameObject sensorGO = Instantiate(Inst.sensorConeVisual, gameObject.transform);
+				sensorGO.transform.localRotation = UnityEngine.Random.rotationUniform;
+				// Make cone shift
+				float distanceBias = UnityEngine.Random.Range(0f, 1f);
+				sensorGO.GetComponent<MeshFilter>().mesh = Inst.GenerateCone(Mathf.Lerp(90f, 10f, distanceBias), Mathf.Lerp(2f, 4f, distanceBias), 20);
+			}
 		} // Ctor.
 
 		public void Update(){
@@ -153,5 +163,35 @@ public class ColonyViewer : MonoBehaviour {
 	public static Quaternion Quat(System.Numerics.Quaternion q) {
         return new Quaternion(q.X, q.Y, q.Z, q.W);
     }
+
+
+
+	public Mesh GenerateCone(float angle, float length, int numSides = 16){
+		Mesh coneMesh = new Mesh();
+		List<Vector3> verts = new List<Vector3>();
+		List<Vector2> uvs = new List<Vector2>();
+
+		// Set verts, UVs
+		verts.Add(Vector3.zero);
+		uvs.Add(Vector2.zero);
+		for(int i = 1; i < numSides + 1; i++){
+			verts.Add(Quaternion.AngleAxis((i - 1) * (360f / (float)numSides), Vector3.forward) * Quaternion.AngleAxis(angle * 0.5f, Vector3.right) * Vector3.forward * length);
+			uvs.Add(Vector2.one);
+		}
+		coneMesh.SetVertices(verts);
+		coneMesh.SetUVs(0, uvs);
+
+		// Set triangles
+		List<int> triangles = new List<int>();
+		for(int i = 1; i < numSides + 1; i++){
+			triangles.Add((int)Mathf.Repeat(i, numSides) + 1);
+			triangles.Add(i);
+			triangles.Add(0);
+		}
+		coneMesh.SetTriangles(triangles, 0);
+
+		return coneMesh;
+	} // End of GenerateCone().
+
 
 } // End of ColonyViewer.
